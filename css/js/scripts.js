47b96f6226df1136b1dbfefb0a1bd48326789513
kemoik <kemoicaleb17@gmail.com>
2023-02-17 11:34:24 +0300

    added items to the marketplace

--- css/js/scripts.js
+++ css/js/scripts.js
@@ -4,3 +4,20 @@ var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
+
+
+(function () {
+  'use strict'
+  var forms = document.querySelectorAll('.needs-validation')
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
